2375c277fe5912f4588a6585acc89704749427b1
Kehinde Ayangbile <kayangbile@softalliance.co.ng>
2020-11-09 14:49:30 +0100

    set up for db port and removed something from film table

--- server/knexfile.js
+++ server/knexfile.js
@@ -10,6 +10,7 @@ module.exports = {
       user:     process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT
     },
     pool: {
       min: 2,
